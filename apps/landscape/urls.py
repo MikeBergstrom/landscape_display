00ebb88d766503eb7fc4256f666e29e77f9f036e
Mike Bergstrom <mikedberg@hotmail.com>
2017-06-16 14:32:48 -0700

    added prime and fail features

--- apps/landscape/urls.py
+++ apps/landscape/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^(?P<num>\d+)$', views.results)
+    url(r'^(?P<num>\d+)$', views.results),
+    url(r'^fail$', views.fail)
 ]
--- apps/landscape/views.py
+++ apps/landscape/views.py
@@ -2,14 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-
+primelist = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
 def index(request):
     return render(request, 'landscape/index.html')
 
 def results(request, num):
     print "in result"
     print num
-    if int(num) <= 10:
+    if int(num) in primelist:
+        print "found prime"
+        img = "prime"
+    elif int(num) <= 10:
         print "10"
         img = "snow"
     elif int(num) <= 20:
@@ -21,10 +24,13 @@ def results(request, num):
     elif int(num) <= 50:
         img = "tropical"
     elif int(num) > 50:
-        return redirect(request, '/fail')
+        return redirect('/fail')
     print img
     imgurl = "landscape/images/"+img+".jpg"
     context = {'imgurl': str(imgurl)}
     print imgurl
     return render(request, 'landscape/results.html', context)
     # Create your views here.
+
+def fail(request):
+    return render(request, 'landscape/fail.html')
